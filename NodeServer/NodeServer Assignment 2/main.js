9b09c29b7367f2d91355f14c23fde07d5eb0670d
speenlok <sanchezbusinessmail@gmail.com>
2023-02-07 17:59:34 +0800

    second assignment

---
+++ NodeServer/NodeServer Assignment 2/main.js
@@ -0,0 +1,50 @@
+const ex = require('express')
+const wd = ex();
+const fs = require('fs');
+
+wd.set("view engine",  "ejs")
+wd.use(ex.urlencoded({ extended: true}))
+
+wd.get('/',(r1, r2)=>{
+    r2.render("login")
+});
+
+wd.get('/login',(r1, r2)=>{
+    r2.render("login")
+});
+
+wd.post('/login-registration', (r1, r2)=>{
+   const username_value = 'admin';
+   const password_value = '123456';
+            
+   let usr = r1.body.username
+   let pwd = r1.body.password
+
+   if(usr == username_value && pwd == password_value){
+    r2.render('form')
+
+   } else {
+    r2.render("login", { errorMessage:"Sorry try again" })
+   }
+
+});
+
+wd.post('/form-registration', (req, res)=>{
+    
+ let fname = req.body.firstname
+ let lname = req.body.lastname
+    
+ console.log(fname +  "    " +lname)
+ 
+    fs.writeFile('storage/' + fname + "-" +lname + '.txt', fname +  "    " +lname, function (err) {
+    if (err) throw err;
+     console.log('Saved!');
+     res.end();
+     
+    }); 
+    
+})
+
+
+wd.listen(3000)
+console.log('running in port 3000')
